c466e0048e39b741009e2cbe283d9a3b65b012b6
Ege Uysal <hello@egeuysal.com>
2025-09-28 17:00:59 -0500

    Update gap size in Landing component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,6 @@
 const Landing: React.FC = () => {
   return (
-    <main className="flex flex-col gap-sm">
+    <main className="flex flex-col gap-2xs">
       <h1>Welcome to Bridge!</h1>
       <p className="md:w-3/4 lg:w-1/2 text-neutral-700 dark:text-neutral-300">
         Share your Markdown files quickly and easily. Upload a file or paste your
