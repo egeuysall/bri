027d9448fbe2c6ea594becdae4967a1b9cc8e3ac
Ege Uysal <hello@egeuysal.com>
2025-09-28 16:48:02 -0500

    Refactor Landing component for improved layout
    Updated the landing page structure and styling.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,12 @@
 const Landing: React.FC = () => {
   return (
-    <main>
-      <h1>
-        Welcome to Bridge. Share your Markdown files quickly and easily. Upload a file or paste your
+    <main className="flex flex-col gap-sm">
+      <h1>Welcome to Bridge!</h1>
+      <p className="md:w-3/4 lg:w-1/2 text-neutral-700 dark:text-neutral-300">
+        Share your Markdown files quickly and easily. Upload a file or paste your
         Markdown and get a clean, shareable link instantly. No accounts needed, no clutter, just
         simple Markdown sharing.
-      </h1>
+      </p>
     </main>
   );
 };
